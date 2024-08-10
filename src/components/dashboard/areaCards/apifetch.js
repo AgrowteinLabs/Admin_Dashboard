2863923dd046d968901eb597ca6dbcca8df286c2
k_.rthik.. <karthikv9778@gmail.com>
2024-08-10 18:58:12 +0530

    Update apifetch.js

--- src/components/dashboard/areaCards/apifetch.js
+++ src/components/dashboard/areaCards/apifetch.js
@@ -2,9 +2,9 @@
 
 export default async function data(){
     try{
-    const Users = await fetch('/api/v1/users');
+    const Users = await fetch('https://agrowteinlabs.onrender.com/api/v1/users');
     const usersdata = await Users.json();
-    const Sensors = await fetch('/api/v1/sensors');
+    const Sensors = await fetch('https://agrowteinlabs.onrender.com/api/v1/sensors');
     const sensorsdata = await Sensors.json();
     return { usersdata, sensorsdata };
 }
