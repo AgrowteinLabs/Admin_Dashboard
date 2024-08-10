cf3b169c1cfa03d4f9175da79bcaff3cafc269e4
k_.rthik.. <karthikv9778@gmail.com>
2024-08-10 18:34:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  esbuild: {
-    target: 'esnext', // or 'es2022'
-  },
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api/': {
+        target: 'https://agrowteinlabs.onrender.com',
+        changeOrigin: true,
+      }
+    }
+  }
 });
