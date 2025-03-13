239a9236af647ebb2286dbd05f1cd3d3a3b4fc09
abhinav bavos <s4abhinav.19@gmail.com>
2025-03-13 17:36:25 +0530

    Error fixed

--- vite.config.js
+++ vite.config.js
@@ -1,44 +1,16 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-=======
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
->>>>>>> alpha
-=======
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
->>>>>>> d499f1d (Initial commit)
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-<<<<<<< HEAD
-<<<<<<< HEAD
       "/api/": {
-        target: "https://agrowteinlabs.onrender.com",
+        target: "https://apiv2.agrowtein.com",
         changeOrigin: true,
         secure: false,  // Set to false if using HTTP
         cookieDomainRewrite: 'localhost',
       },
     },
   },
-=======
-      '/api/': {
-        target: 'https://agrowteinlabs.onrender.com',
-        changeOrigin: true,
-      }
-    }
-  }
->>>>>>> alpha
-=======
-      '/api/': {
-        target: 'https://agrowteinlabs.onrender.com',
-        changeOrigin: true,
-      }
-    }
-  }
->>>>>>> d499f1d (Initial commit)
 });
